b08295665cf1883f1bd696520971be32a6f6c640
pepe <pepe@ender4.dsic.upv.es>
2009-02-26 16:10:34 +0100

    Initial contribution
    Ignore-this: ec131e24facae940fdcc024913557abc
    
    darcs-hash:20090226151034-26539-ccf115c1108bb9b49b843639a7714d359a260b2f.gz

---
+++ Language/Prolog/Signature.hs
@@ -0,0 +1,22 @@
+
+module Language.Prolog.Signature (module TRS.Signature) where
+
+import Control.Monad
+import qualified Data.Foldable as F
+import Data.Monoid
+import qualified Data.Set as Set
+import Data.Set (Set)
+import qualified Data.Map as Map
+import Language.Prolog.Syntax
+import TRS.Signature
+
+
+deriving instance Ord VName
+
+instance SignatureC Program Ident where
+  getSignature cc = let aritiesP = Map.fromList [ (f, length tt) | Pred f tt   <- F.toList =<< cc]
+                        aritiesF = Map.fromList [ (f, length tt) | Pred _ args <- F.toList =<< cc, Term f tt <- subterms =<< args ]
+                        functors = Map.keysSet aritiesF
+                        preds    = Map.keysSet aritiesP
+                        subterms (In t) = t : (F.concatMap subterms t)
+                        in Sig {constructorSymbols = functors, definedSymbols = preds, arity = aritiesP `mappend` aritiesF}
